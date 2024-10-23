57587151a57d23fd097508c75dfbbca66e7324ac
Trần Minh Tuấn <tuantranminh500@gmail.com>
2024-10-23 17:09:31 +0700

    phân quyền

---
+++ WebTrangSuc/WebTrangSuc/Areas/XuLyDonHang/XuLyDonHangAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebTrangSuc.Areas.XuLyDonHang
+{
+    public class XuLyDonHangAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "XuLyDonHang";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "XuLyDonHang_default",
+                "XuLyDonHang/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
