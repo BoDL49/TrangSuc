1cc086617442299715ee47e913b2828bcc501219
Trần Minh Tuấn <tuantranminh500@gmail.com>
2024-10-23 16:51:57 +0700

    update
    vẫn còn lỗi layout
    Trần Minh Tuấn

---
+++ WebTrangSuc/WebTrangSuc/Controllers/ChiTietSanPhamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTrangSuc.Controllers
+{
+    public class ChiTietSanPhamController : Controller
+    {
+        // GET: ChiTietSanPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
